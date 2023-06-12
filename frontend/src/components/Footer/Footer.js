f93f2a6a4f981116665aa13d797aef4ed504649e
Francis Baissie <baissiefrancis8@gmail.com>
2023-06-12 15:21:07 +0000

    save

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './Footer.css'
 
 const Footer = () => {
+
+  const currYear = new Date().getFullYear();
   return (
     <section className='footer-wrapper'>
     <div className='flexCenter footer-container'>
     <div >
       <div className='footer-line'></div>
-      <p>@ 2023 KNUST EXAMINATIONS CENTER | All Rights Reserved</p>
+      <p>@ {currYear} KNUST EXAMINATIONS CENTER | All Rights Reserved</p>
     
     </div>
     </div>
