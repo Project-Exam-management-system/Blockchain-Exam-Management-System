552323a82942302b09d47227bda53aa182c28a2f
anatswei <anatswei01@gmail.com>
2023-06-12 15:00:59 +0000

    added popup and lecturer page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import Choose from './components/Choose/Choose';
-import Footer from './components/Footer/Footer';
-import Hero from './components/Hero/Hero'
-
+import "./App.css";
+import Choose from "./components/Choose/Choose";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Lecturer from "./components/Lecturer/Lecturer";
 function App() {
   return (
     <div className="App">
-      <Hero/>
-      <Choose/>
-      <Footer/>
+      <Hero />
+      <Choose />
+      <Lecturer />
+      <Footer />
     </div>
   );
 }
